bdf26927e8b65944443766787f3efe579addd8e0
quan2804 <boxgame7406@gmail.com>
2026-04-03 21:11:51 +0700

    Update pet.ts

--- frontend/src/data/pet.ts
+++ frontend/src/data/pet.ts
@@ -1,67 +1,18 @@
 export type Pet = {
-  id: number
+  id: string
   name: string
   age: number
   gender: string
   image: string
-  type: string
+  type?: string
+  category: string | {
+    id: string
+    name: string
+    status?: string
+  }
   sterilized: boolean
+  vaccinated: boolean
   color: string
+  description: string
+  status?: string
 }
-// Du lieu: Tiem chung
-
-export const pets: Pet[] = [
-
-  {
-    id: 1,
-    name: "Alaska",
-    age: 1,
-    gender: "Male",
-    type: "dog",
-    image: "/images/Jack.png",
-    sterilized: true,
-    color: "white"
-  },
-  {
-    id: 2,
-    name: "Malley",
-    age: 10,
-    gender: "Female",
-    type: "cat",
-    image: "/images/Malley.png",
-    sterilized: false,
-    color: "gray"
-  },
-  {
-    id: 2,
-    name: "Malley",
-    age: 10,
-    gender: "Female",
-    type: "cat",
-    image: "/images/Malley.png",
-    sterilized: false,
-    color: "gray"
-  }
-  ,
-  {
-    id: 2,
-    name: "Malley",
-    age: 10,
-    gender: "Female",
-    type: "cat",
-    image: "/images/Malley.png",
-    sterilized: false,
-    color: "gray"
-  },
-  {
-    id: 2,
-    name: "Malley",
-    age: 10,
-    gender: "Female",
-    type: "cat",
-    image: "/images/Malley.png",
-    sterilized: false,
-    color: "gray"
-  }
- 
-]
