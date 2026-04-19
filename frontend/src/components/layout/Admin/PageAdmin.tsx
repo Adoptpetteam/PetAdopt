0cdcd0f2292f0e6a5e577cd3e0c6557aba6d2ac0
Tùng Đỗ <kristensendo.26@gmail.com>
2026-04-19 12:28:34 +0700

    Add/update frontend

---
+++ frontend/src/components/layout/Admin/PageAdmin.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Outlet } from "react-router-dom";
+import HeaderAdmin from "./HeaderAdmin";
+import SideBarAdmin from "./SideBarAdmin";
+import "./cssAdmin.css"; 
+
+const PageAdmin = () => {
+  return (
+    <div className="admin-layout-container">
+      {/* 1. SIDEBAR CHIẾM CỘT TRÁI */}
+      <SideBarAdmin />
+
+      {/* 2. WRAPPER CHIẾM CỘT PHẢI */}
+      <div className="admin-right-wrapper">
+        <HeaderAdmin />
+        
+        <main className="admin-content-main">
+             <Outlet />
+             
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default PageAdmin;
