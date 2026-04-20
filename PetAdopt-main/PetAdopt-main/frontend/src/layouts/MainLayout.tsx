4db956ce94fbd64a25cb13ffe37cd6f4db253932
Trunghuy89 <huyntph52547@gmail.com>
2026-04-20 23:13:13 +0700

    update MainLayout

--- PetAdopt-main/PetAdopt-main/frontend/src/layouts/MainLayout.tsx
+++ PetAdopt-main/PetAdopt-main/frontend/src/layouts/MainLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom"
 import Footer from "../components/layout/Footer"
 import Header from "../components/layout/Header"
 
+
 export default function MainLayout() {
   return (
     <>
