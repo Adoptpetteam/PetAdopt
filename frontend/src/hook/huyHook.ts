5baf5481b93be6b7bce077e059c6b961c69c7c88
Trunghuy89 <huyntph52547@gmail.com>
2026-04-24 12:16:34 +0700

    Cập nhật custom hooks huyHook lên main

--- frontend/src/hook/huyHook.ts
+++ frontend/src/hook/huyHook.ts
@@ -3,12 +3,22 @@ import { useNavigate } from "react-router-dom";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 import {
+  createAdoption,
+  createPet,
+  deleteAdoption,
+  deletePet,
   getCreateCategory,
   getDeleteCategory,
+  getListAdoption,
   getListCategory,
+  getListPet,
+  getPetDetail,
   getUpdateCategory,
+  updatePet,
   type Props,
 } from "../provider/huyProvider";
+
+import type { Pet } from "../data/pet"
 
 // lấy danh sách
 export const useListCategory = ({ resource = "category" }: Props) => {
@@ -89,6 +99,116 @@ export const useUpdateCategory = ({ resource = "category" }: Props) => {
       } else {
         message.error("Cập nhật thất bại");
       }
+    },
+  });
+};
+
+// ** Tùng **
+
+// PETS
+// Lấy danh sách thú cưng
+export const useListPet = ({ resource = "pets" }: Props) => {
+  return useQuery<Pet[]>({
+    queryKey: [resource],
+    queryFn: () => getListPet({ resource }),
+  });
+};
+
+// Chi tiết thú cưng
+export const usePetDetail = ({ resource = "pets", id }: any) => {
+  return useQuery<Pet>({
+    queryKey: [resource, id],
+    queryFn: () => getPetDetail({ resource, id }),
+    enabled: !!id, // chỉ call khi có id
+  });
+};
+
+
+
+// Adoption
+export const useCreateAdoption = ({ resource = "adoptions" }: any) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (values: any) =>
+      createAdoption({ resource, values }),
+
+    onSuccess: () => {
+      message.success("Gửi form thành công!")
+      queryClient.invalidateQueries({ queryKey: [resource] })
+    },
+
+    onError: () => {
+      message.error("Gửi form thất bại")
+    },
+  })
+}
+
+// Admin Adoption
+// lấy danh sách
+export const useListAdoption = ({ resource = "adoptions" }: any) => {
+  return useQuery({
+    queryKey: [resource],
+    queryFn: () => getListAdoption({ resource }),
+  })
+}
+
+// xóa
+export const useDeleteAdoption = ({ resource = "adoptions" }: any) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (id: number) =>
+      deleteAdoption({ resource, id }),
+
+    onSuccess: () => {
+      message.success("Xóa thành công")
+      queryClient.invalidateQueries({ queryKey: [resource] })
+    },
+
+    onError: () => {
+      message.error("Xóa thất bại")
+    },
+  })
+}
+
+// CREATE
+export const useCreatePet = ({ resource = "pets" }: Props) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (values: any) => createPet({ resource, values }),
+    onSuccess: () => {
+      message.success("Thêm thú cưng thành công");
+      queryClient.invalidateQueries({ queryKey: [resource] });
+    },
+  });
+};
+
+// UPDATE
+export const useUpdatePet = ({ resource = "pets" }: Props) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ id, values }: any) =>
+      updatePet({ resource, id, values }),
+    onSuccess: () => {
+      message.success("Cập nhật thành công");
+      queryClient.invalidateQueries({ queryKey: [resource] });
+    },
+  });
+};
+
+// DELETE
+export const useDeletePet = ({ resource = "pets" }: Props) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (id: string | number) =>
+      deletePet({ resource, id }),
+    onSuccess: () => {
+      message.success("Xóa thành công");
+      queryClient.invalidateQueries({ queryKey: [resource] });
     },
   });
 };
